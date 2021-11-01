fd49766136197ddc5278de34c7692138da285cad
V-Martyanov <vladimirmartalex@gmail.com>
2021-11-01 22:30:02 +0000

    Update String.cpp

--- string/String.cpp
+++ string/String.cpp
@@ -1,49 +1,49 @@
 #include "String.hpp"
 
 String::String(const size_t kSiz, const char &symbol) {
-  this->str_ = new char[kSiz + 1];
+  str_ = new char[kSiz + 1];
   for (size_t i = 0; i < kSiz; ++i) {
-    this->str_[i] = symbol;
+    str_[i] = symbol;
   }
-  this->capacity_ = kSiz + 1;
-  this->size_ = kSiz;
+  capacity_ = kSiz + 1;
+  size_ = kSiz;
 }
 
 String::String() {
-  this->str_ = nullptr;
-  this->capacity_ = 1;
+  str_ = nullptr;
+  capacity_ = 1;
 }
 
 String::String(const String &str) {
   size_t len = str.Size();
-  this->str_ = new char[len + 1];
+  str_ = new char[len + 1];
   for (size_t i = 0; i < len; ++i) {
-    this->str_[i] = str.str_[i];
+    str_[i] = str.str_[i];
   }
-  this->capacity_ = len + 1;
-  this->size_ = len;
+  capacity_ = len + 1;
+  size_ = len;
 }
 
 String::String(const char* str) {
   int len = strlen(str);
-  this->str_ = new char[len + 1];
+  str_ = new char[len + 1];
   for (int i = 0; i < len; ++i) {
-    this->str_[i] = str[i];
+    str_[i] = str[i];
   }
-  this->capacity_ = len + 1;
-  this->size_ = len;
+  capacity_ = len + 1;
+  size_ = len;
 }
 
 String::String(char symbol) {
-  this->str_ = new char[2];
-  this->str_[0] = symbol;
-  this->size_ = 1;
-  this->capacity_ = 2;
+  str_ = new char[2];
+  str_[0] = symbol;
+  size_ = 1;
+  capacity_ = 2;
 }
 
 void String::Swap(String &other) {
-  char* temp = this->str_;
-  this->str_ = other.str_;
+  char* temp = str_;
+  str_ = other.str_;
   other.str_ = temp;
 }
 
@@ -65,18 +65,18 @@ std::ostream &operator<<(std::ostream &out, const String &str) {
 
 String &String::operator=(const String &other) {
   // Data
-  if (this->str_ == other.str_) {
+  if (str_ == other.str_) {
     return *this;
   }
   if (this != &other) {
-    delete[] this->str_;
+    delete[] str_;
   }
-  this->str_ = new char[other.Size() + 1];
+  str_ = new char[other.Size() + 1];
   for (size_t i = 0; i < other.Size(); ++i) {
-    this->str_[i] = other.str_[i];
+    str_[i] = other.str_[i];
   }
-  this->capacity_ = other.Size() + 1;
-  this->size_ = other.Size();
+  capacity_ = other.Size() + 1;
+  size_ = other.Size();
   return *this;
 }
 
@@ -103,66 +103,66 @@ bool operator<(const String &str2, const String &str1) {
 
 void String::ShrinkToFit() {
   if (size_ < capacity_ - 1) {
-    char* strt = new char[this->size_ + 1];
+    char* strt = new char[size_ + 1];
     size_t i = 0;
-    for (; i < this->size_; ++i) {
-      strt[i] = this->str_[i];
+    for (; i < size_; ++i) {
+      strt[i] = str_[i];
     }
-    delete[] this->str_;
-    this->str_ = strt;
-    this->capacity_ = this->size_ + 1;
+    delete[] str_;
+    str_ = strt;
+    capacity_ = size_ + 1;
   }
 }
 
 void String::Reserve(size_t new_cap) {
-  if (this->capacity_ < new_cap) {
+  if (capacity_ < new_cap) {
     char* strt = new char[new_cap * 2 + 1];
     size_t i = 0;
-    for (; i < this->size_; ++i) {
-      strt[i] = this->str_[i];
+    for (; i < size_; ++i) {
+      strt[i] = str_[i];
     }
-    delete[] this->str_;
-    this->str_ = strt;
-    this->capacity_ = 2 * new_cap + 1;
+    delete[] str_;
+    str_ = strt;
+    capacity_ = 2 * new_cap + 1;
   }
 }
 
 void String::Resize(size_t new_size, char symbol) {
-  if (new_size >= this->capacity_) {
+  if (new_size >= capacity_) {
     char* strt = new char[new_size + 1];
     size_t i = 0;
-    for (; i < this->size_; ++i) {
-      strt[i] = this->str_[i];
+    for (; i < size_; ++i) {
+      strt[i] = str_[i];
     }
-    for (size_t j = 0; j < new_size - this->size_; j++, i++) {
+    for (size_t j = 0; j < new_size - size_; j++, i++) {
       strt[i] = symbol;
     }
-    if (this->size_ > 0) {
-      delete[] this->str_;
+    if (size_ > 0) {
+      delete[] str_;
     }
-    this->str_ = strt;
-    this->size_ = new_size;
-    this->capacity_ = new_size + 1;
+    str_ = strt;
+    size_ = new_size;
+    capacity_ = new_size + 1;
   } else {
-    this->size_ = new_size;
+    size_ = new_size;
   }
 }
 
 void String::Resize(size_t new_size) {
-  if (new_size >= this->capacity_) {
+  if (new_size >= capacity_) {
     char* strt = new char[new_size + 1];
     size_t i = 0;
-    for (; i < this->size_; ++i) {
-      strt[i] = this->str_[i];
+    for (; i < size_; ++i) {
+      strt[i] = str_[i];
     }
-    if (this->size_ > 0) {
-      delete[] this->str_;
+    if (size_ > 0) {
+      delete[] str_;
     }
-    this->str_ = strt;
-    this->size_ = new_size;
-    this->capacity_ = new_size + 1;
+    str_ = strt;
+    size_ = new_size;
+    capacity_ = new_size + 1;
   } else {
-    this->size_ = new_size;
+    size_ = new_size;
   }
 }
 
@@ -190,24 +190,24 @@ String operator*(String str, unsigned int m) {
 
 String &String::operator+=(const String &other) {
   size_t len = other.Size();
-  if (this->capacity_ <= (this->size_ + len + 1)) {
-    char* strt = new char[this->size_ + len + 1];
+  if (capacity_ <= (size_ + len + 1)) {
+    char* strt = new char[size_ + len + 1];
     size_t i = 0;
-    for (; i < this->size_; ++i) {
-      strt[i] = this->str_[i];
+    for (; i < size_; ++i) {
+      strt[i] = str_[i];
     }
     for (size_t j = 0; j < len; j++, i++) {
       strt[i] = other.str_[j];
     }
-    delete[] this->str_;
-    this->str_ = strt;
-    this->size_ = len + this->size_;
-    this->capacity_ = this->size_ + 1;
+    delete[] str_;
+    str_ = strt;
+    size_ = len + size_;
+    capacity_ = size_ + 1;
   } else {
-    for (size_t i = this->size_, j = 0; j < len; ++i, ++j) {
-      this->str_[i] = other.str_[j];
+    for (size_t i = size_, j = 0; j < len; ++i, ++j) {
+      str_[i] = other.str_[j];
     }
-    this->size_ += len;
+    size_ += len;
   }
   return *this;
 }
@@ -219,51 +219,54 @@ String operator+(const String &other1, const String &other2) {
 }
 
 void String::PopBack() {
-  if (this->size_ == 0) {
+  if (size_ == 0) {
     return;
   }
   --size_;
 }
 
-size_t String::Capacity() const { return this->capacity_ - 1; }
+size_t String::Capacity() const { return capacity_ - 1; }
 
 void String::PushBack(const char kSymbol) {
-  if (this->size_ >= (this->capacity_ - 1)) {
+  if (size_ >= (capacity_ - 1) && size_ > 1) {
     char* strt = nullptr;
-    if (this->size_ == 0) {
-      if (capacity_ == 1) {
-        strt = new char[capacity_ * 2 + 1];
-        capacity_ *= 2 + 1;
-      } else {
-        strt = new char[(capacity_ - 1) * 2 + 1];
-        capacity_ = (capacity_ - 1) * 2 + 1;
-      }
-    } else {
-      strt = new char[(capacity_ - 1) * 2 + 1];
-      capacity_ = (capacity_ - 1) * 2 + 1;
-    }
+    strt = new char[size_ * 2 + 1];
+    capacity_ = size_ * 2 + 1;
     for (size_t i = 0; i < size_; ++i) {
-      strt[i] = this->str_[i];
+      strt[i] = str_[i];
     }
-    delete[] this->str_;
-    this->str_ = strt;
-    this->str_[size_] = kSymbol;
+    delete[] str_;
+    str_ = strt;
+    str_[size_] = kSymbol;
     ++size_;
   } else {
-    this->str_[size_] = kSymbol;
-    this->size_ += 1;
+    if (size_ == 0) {
+      delete str_;
+      if (capacity_ == 1) {
+        str_ = new char[2];
+        capacity_ = 2;
+      } else {
+        str_ = new char[(capacity_ - 1) * 2 + 1];
+        capacity_ = (capacity_ - 1) * 2 + 1;
+      }
+      str_[0] = kSymbol;
+      ++size_;
+    } else {
+      str_[size_] = kSymbol;
+      ++size_;
+    }
   }
 }
 
-size_t String::Size() const { return this->size_; }
+size_t String::Size() const { return size_; }
 
-void String::Clear() { this->size_ = 0; }
+void String::Clear() { size_ = 0; }
 
 const char* String::Data() const {
-  if (this->size_ > 0) {
-    this->str_[this->size_] = '\0';
+  if (size_ > 0) {
+    str_[size_] = '\0';
   }
-  return this->str_;
+  return str_;
 }
 
 String String::Join(const std::vector<String> &vec) const {
@@ -286,7 +289,7 @@ void String::SplitHelpSize(String &temp, const String &str_div,
                            size_t &str_point, String &t, vector<String> &ret) {
   int pol = 0;
   int iter = 0;
-  for (int iq = 0; iq < Size(); ++iq) {
+  for (size_t iq = 0; iq < Size(); ++iq) {
     if (t == str_div) {
       for (size_t i = 0; i < str_div.Size(); ++i) {
         temp.PopBack();
@@ -294,9 +297,9 @@ void String::SplitHelpSize(String &temp, const String &str_div,
       ret.push_back(temp);
       temp.Clear();
       t.Clear();
-      if (str_point < this->size_) {
+      if (str_point < size_) {
         for (size_t i = 0; i < str_div.Size(); ++i) {
-          t += this->str_[str_point];
+          t += str_[str_point];
           ++str_point;
         }
       }
@@ -304,11 +307,10 @@ void String::SplitHelpSize(String &temp, const String &str_div,
       ++pol;
       ++iter;
     }
-    // проверка, что нет str_div, которые идут подряд, если - да,
-    // то удаляем все в одном цикле, если нет, то выходим
     if (iter == 2 && pol <= 1) {
       break;
-    } else {
+    }
+    if (iter == 2) {
       pol = 0;
       iter = 0;
     }
@@ -319,12 +321,12 @@ vector<String> String::Split(const String &str_div) {
   vector<String> ret;
   String str_push;
   String div_temp;
-  if (this->size_ == 0) {
+  if (size_ == 0) {
     ret.push_back("");
     return ret;
   }
   for (size_t i = 0; i < str_div.Size(); i++) {
-    div_temp += this->str_[i];
+    div_temp += str_[i];
   }
   str_push = div_temp;
   size_t str_point = str_push.Size();
@@ -332,8 +334,8 @@ vector<String> String::Split(const String &str_div) {
     SplitHelpSize(str_push, str_div, str_point, div_temp, ret);
     str_push.PushBack(str_[str_point]);
     ++str_point;
-    for (size_t k = str_push.Size() - str_div.Size(), i = 0; k < str_push.Size();
-         ++k, ++i) {
+    for (size_t k = str_push.Size() - str_div.Size(), i = 0;
+         k < str_push.Size(); ++k, ++i) {
       div_temp[i] = str_push[k];
     }
   }
@@ -343,4 +345,3 @@ vector<String> String::Split(const String &str_div) {
   div_temp.Clear();
   return ret;
 }
-
