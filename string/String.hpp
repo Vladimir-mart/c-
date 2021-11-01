21c460855e36c79ae24ae3ae75c487c32155e542
V-Martyanov <vladimirmartalex@gmail.com>
2021-11-01 22:51:06 +0000

    Update String.hpp

--- string/String.hpp
+++ string/String.hpp
@@ -13,15 +13,15 @@ using std::vector;
 class String {
  public:
   String(const char* str);
-  String(const String &);
+  String(const String&);
   String();
   String(char);
-  String(size_t, const char &symbol);
+  String(size_t, const char& symbol);
   ~String();
 
-  String &operator=(const String &);
-  String &operator+=(const String &);
-  String &operator*=(int);
+  String& operator=(const String&);
+  String& operator+=(const String&);
+  String& operator*=(int);
 
   void Resize(size_t new_size);
   void Resize(size_t new_size, char symbol);
@@ -34,30 +34,30 @@ class String {
 
   void PopBack();
   void PushBack(char);
-  void Swap(String &other);
+  void Swap(String& other);
 
-  char &operator[](const size_t kIndex) { return this->str_[kIndex]; }
-  const char &operator[](const size_t kIndex) const {
+  char& operator[](const size_t kIndex) { return this->str_[kIndex]; }
+  const char& operator[](const size_t kIndex) const {
     return this->str_[kIndex];
   }
 
-  char &Front() { return this->str_[0]; }
-  const char &Front() const { return this->str_[0]; }
-  char &Back() { return this->str_[size_ - 1]; }
-  const char &Back() const { return this->str_[size_ - 1]; }
+  char& Front() { return this->str_[0]; }
+  const char& Front() const { return this->str_[0]; }
+  char& Back() { return this->str_[size_ - 1]; }
+  const char& Back() const { return this->str_[size_ - 1]; }
   bool Empty() const { return size_ == 0; }
 
-  bool operator<=(const String &) const;
-  bool operator>=(const String &) const;
-  bool operator==(const String &) const;
-  bool operator!=(const String &) const;
+  bool operator<=(const String&) const;
+  bool operator>=(const String&) const;
+  bool operator==(const String&) const;
+  bool operator!=(const String&) const;
 
-  friend std::ostream &operator<<(std::ostream &, const String &);
-  friend std::istream &operator>>(std::istream &, String &);
+  friend std::ostream& operator<<(std::ostream&, const String&);
+  friend std::istream& operator>>(std::istream&, String&);
 
-  void SplitHelpSize(String &, const String &, size_t &, String &, vector<String> &);
-  vector<String> Split(const String &str_div = " ");
-  String Join(const std::vector<String> &vec) const;
+  void SplitHelpSize(String&, const String&, size_t&, String&, vector<String>&);
+  vector<String> Split(const String& str_div = " ");
+  String Join(const std::vector<String>& vec) const;
 
  private:
   char* str_ = nullptr;
@@ -66,8 +66,8 @@ class String {
 
 };
 
-bool operator<(const String &, const String &);
-bool operator>(const String &, const String &);
+bool operator<(const String&, const String&);
+bool operator>(const String&, const String&);
 
 String operator*(String str, unsigned int m);
-String operator+(const String &other1, const String &other2);
+String operator+(const String& other1, const String& other2);
