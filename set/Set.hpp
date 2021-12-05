11f4ee1b95f67c468846f5e9833dc4a862515265
V-Martyanov <vladimirmartalex@gmail.com>
2021-12-05 06:43:35 +0000

    Update Set.hpp

--- set/Set.hpp
+++ set/Set.hpp
@@ -5,46 +5,41 @@
 template <typename Key, typename C = std::less<Key>>
 class Set {
 public:
-  // NO LINT
-  typedef size_t        size_type;
-  // NO LINT
-  typedef Key           key_type;
-  // NO LINT
-  typedef key_type      value_type;
-  // NO LINT
-  typedef C             key_compare;
-  // NO LINT
-  typedef key_compare   value_compare;
+  typedef size_t        SizeType;
+  typedef Key           KeyType;
+  typedef KeyType       ValueType;
+  typedef C             KeyCompare;
+  typedef KeyCompare    ValueCompare;
   Set() = default;
   Set(const Set&);
   Set& operator=(const Set&);
   ~Set();
-  size_type Size() const;
+  SizeType Size() const;
   bool Empty() const;
   void Clear();
-  void Insert(const value_type&);
-  void Erase(const value_type&);
-  bool Find(const value_type&) const;
+  void Insert(const ValueType&);
+  void Erase(const ValueType&);
+  bool Find(const ValueType&) const;
   
 private:
   struct Node {
     Node(const Key&);
     Node* left = nullptr;
     Node* right = nullptr;
-    value_type val = 0;
+    ValueType val = 0;
     int priority = 0;
-    size_type amount = 1;
+    SizeType amount = 1;
   };
   static int GetRandomNumber();
-  Node* Build(value_type*, size_type);
+  Node* Build(ValueType*, SizeType);
   void Heapify(Node* root);
-  bool Compare(const value_type&, const value_type&, const C& comp = C()) const;
-  size_type GetAmount(Node*) const;
+  bool Compare(const ValueType&, const ValueType&, const C& comp = C()) const;
+  SizeType GetAmount(Node*) const;
   void UpdateAmount(Node*);
-  void EraseRecursive(Node*&, const value_type&);
+  void EraseRecursive(Node*&, const ValueType&);
   void ClearRecursive(Node*);
   Node* Merge(Node*, Node*);
-  void Split(Node*, const value_type&, Node*&, Node*&);
-  bool FindRecursive(Node*, const value_type&) const;
+  void Split(Node*, const ValueType&, Node*&, Node*&);
+  bool FindRecursive(Node*, const ValueType&) const;
   Node* root_ = nullptr;
 };
