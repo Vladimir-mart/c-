26c9dbcd2758c0a6bfbf8b9412d2be1cda72cdd7
V-Martyanov <vladimirmartalex@gmail.com>
2021-12-07 10:01:55 +0000

    Update Set.hpp

--- set/Set.hpp
+++ set/Set.hpp
@@ -6,11 +6,11 @@
 template <typename Key, typename C = std::less<Key>>
 class Set {
  public:
-  typedef size_t size_type; //nolint
-  typedef Key key_type; //nolint
-  typedef key_type value_type; //nolint
-  typedef C key_compare; //nolint
-  typedef key_compare ValueCompare; //nolint
+  typedef size_t size_type; //nolint:golint,unused
+  typedef Key key_type; //nolint:golint,unused
+  typedef key_type value_type; //nolint:golint,unused
+  typedef C key_compare; //nolint:golint,unused
+  typedef key_compare ValueCompare; //nolint:golint,unused
   Set() = default;
   Set(const Set&);
   Set& operator=(const Set&);
